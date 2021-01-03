0067c9408b9cc31fe8e3fdda10515216c0d8fa57
Ostap Dutka <71087467+Ostap2003@users.noreply.github.com>
2021-01-03 19:02:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from read_write_file import read_matrix_file, write_matrix, write_relation
 from find_closure import find_reflexive_relation, find_symmetric_relation
 from transitive_counter import count_transitive
-from transitive_relations import write_matrix_to_file, write_rel_to_file, check_transition
+from transitive_relations import warshall_alg, check_transition
 from equiv_classes import find_equiv_classes, matrix_to_relations
