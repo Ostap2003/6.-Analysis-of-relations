2d789730eea12f65870c024e65beb2184a41d097
Tania Prysiazhnyk <73783964+Tanchyyk@users.noreply.github.com>
2021-01-03 18:43:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from read_file import read_matrix_file
+from write_file import write_matrix, write_relation
+from find_reflexive_relation import find_reflexive_relation
+from find_symetric_relation import find_symmetric_relation
+from transitive_counter import count_transitive
+from transitive_relations import write_matrix_to_file, write_rel_to_file, check_transition
+from equiv_classes import find_equiv_classes, matrix_to_relations
