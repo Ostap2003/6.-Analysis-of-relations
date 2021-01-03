079b0d471b3cc70e5be35ae0f60d2949e28a6c57
44ust14 <ustym.hanyk@ukr.net>
2021-01-03 21:22:39 +0200

    Changed project structure

--- code/main.py
+++
@@ -1,5 +0,0 @@
-from read_write_file import read_matrix_file, write_matrix, write_relation
-from find_closure import find_reflexive_relation, find_symmetric_relation
-from transitive_counter import count_transitive
-from transitive_relations import warshall_alg, check_transition
-from equiv_classes import find_equiv_classes, matrix_to_relations
---
+++ main.py
@@ -0,0 +1,5 @@
+from modules.read_write_file import read_matrix_file, write_matrix, write_relation
+from modules.find_closure import find_reflexive_relation, find_symmetric_relation
+from modules.transitive_counter import count_transitive
+from modules.transitive_relations import warshall_alg, check_transition
+from modules.equiv_classes import find_equiv_classes, matrix_to_relations
